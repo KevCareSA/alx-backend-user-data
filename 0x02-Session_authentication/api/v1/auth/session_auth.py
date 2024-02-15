a91f3e64acb6ba4c442540895fc6235aa5507827
KevCareSA <kev.mkbn2@gmail.com>
2024-02-15 10:48:01 +0200

    session auth

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+"""
+Handles session authorization and authentication
+
+"""
+from api.v1.auth.auth import Auth
+from uuid import uuid4
+from models.user import User
+
+
+class SessionAuth(Auth):
+    """Handles session authorization and authentication"""
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """ creates a Session ID for a user_id
+        """
+        if (user_id is None or type(user_id) is not str):
+            return None
+        session_id = str(uuid4())
+        SessionAuth.user_id_by_session_id[session_id] = user_id
+        return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """ returns a User ID based on a Session ID
+        """
+        if (session_id is None or type(session_id) is not str):
+            return None
+        return SessionAuth.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """ (overload) that returns a User instance based on a cookie value
+        """
+        session_id = self.session_cookie(request)
+        user_id = self.user_id_by_session_id.get(session_id)
+        return User.get(user_id)
+
+    def destroy_session(self, request=None):
+        """ Deletes user session / login(out)
+        """
+        cookie_data = self.session_cookie(request)
+        if cookie_data is None:
+            return False
+        if not self.user_id_for_session_id(cookie_data):
+            return False
+        del self.user_id_by_session_id[cookie_data]
+        return True
